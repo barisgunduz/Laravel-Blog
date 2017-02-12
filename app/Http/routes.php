32e025d2547412a5115844d262f023d6858d4693
Barış Gündüz <baris.gunduz7@gmail.com>
2017-02-12 14:16:26 +0200

    Restart

---
+++ app/Http/routes.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/blog', [
+    'uses' => 'BlogController@index',
+    'as'   => 'blog'
+]);
+
+Route::get('/blog/{post}', [
+    'uses' => 'BlogController@show',
+    'as'   => 'blog.show'
+]);
+
+Route::get('/category/{category}', [
+    'uses' => 'BlogController@category',
+    'as'   => 'category'
+]);
+
+Route::get('/author/{author}', [
+    'uses' => 'BlogController@author',
+    'as'   => 'author'
+]);
+
+Route::auth();
+
+Route::get('/home', 'Backend\HomeController@index');
+
+Route::put('/backend/blog/restore/{blog}', [
+    'uses' => 'Backend\BlogController@restore',
+    'as'   => 'backend.blog.restore'
+]);
+Route::delete('/backend/blog/force-destroy/{blog}', [
+    'uses' => 'Backend\BlogController@forceDestroy',
+    'as'   => 'backend.blog.force-destroy'
+]);
+Route::resource('/backend/blog', 'Backend\BlogController');
+
+Route::resource('/backend/categories', 'Backend\CategoriesController');
+
+Route::get('/backend/users/confirm/{users}', [
+    'uses' => 'Backend\UsersController@confirm',
+    'as' => 'backend.users.confirm'
+]);
+Route::resource('/backend/users', 'Backend\UsersController');
+
